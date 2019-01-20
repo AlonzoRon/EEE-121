c28f2011876957fac17dc7ad9f406ef92e71acb0
AlonzoRon <ron.alonzo@eee.upd.edu.ph>
2019-01-21 03:07:32 +0800

    edited exercise 1.py to show range also edited meeting 1 to meeting_1 same for meeting 2..

--- Meeting_2/exercise1.py
+++ Meeting_2/exercise1.py
@@ -1,22 +1,29 @@
 def g(x, n):
     return (x ** 2 + 1) % n
- 
+
+
 def gcd(m, n):
     if m < 0:
         m = -m
     if n < 0:
         n = -n
-     
+
     if n == 0:
         return m
     return gcd(n, m % n)
- 
+
+
 def maybe_prime(n):
     x, y, d = 2, 2, 1
-     
+
     while d == 1:
         x = g(x, n)
         y = g(g(y, n), n)
         d = gcd(x - y, n)
-     
+
     return d == n
+
+
+for i in range(100, 200):
+    if maybe_prime(i):
+        print(i)
