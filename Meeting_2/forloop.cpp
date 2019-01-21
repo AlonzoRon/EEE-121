3e838a7dd2f587cae6b31fc2d5223dfc4eb8e266
AlonzoRon <ron.alonzo@eee.upd.edu.ph>
2019-01-21 18:30:52 +0800

    changed '=' to '=='

--- Meeting_2/forloop.cpp
+++ Meeting_2/forloop.cpp
@@ -2,42 +2,42 @@
 using namespace std;
 
 int main() {
-    
+
     int a;
     int b;
 	int division;
-	
+
 	cin >> a >> b;
 
     for(int i = a; i <= b; i++){
 
 		if(i <= 9){
 
-			if(i = 1){
+			if(i == 1){
 				cout << "one" << endl;
 			}
-			if(i = 2){
+			if(i == 2){
 				cout << "two" << endl;
 			}
-			if(i = 3){
+			if(i == 3){
 				cout << "three" << endl;
 			}
-			if(i = 4){
+			if(i == 4){
 				cout << "four" << endl;
 			}
-			if(i = 5){
+			if(i == 5){
 				cout << "five" << endl;
 			}
-			if(i = 6){
+			if(i == 6){
 				cout << "six" << endl;
 			}
-			if(i = 7){
+			if(i == 7){
 				cout << "seven" << endl;
 			}
-			if(i = 8){
+			if(i == 8){
 				cout << "eight" << endl;
 			}
-			if(i = 9){
+			if(i == 9){
 				cout << "nine" << endl;
 			}
 		}
@@ -53,6 +53,6 @@ int main() {
 				}
 			}
 		}
-		
+
 	}
 }
