0ddf6567316e62e8cb2e57c5d0e69ef928b25ce7
AlonzoRon <ron.alonzo@eee.upd.edu.ph>
2019-02-02 22:36:51 +0800

    made command parser and completed querydate

--- Librarby/librarby.cpp
+++ Librarby/librarby.cpp
@@ -64,16 +64,7 @@ string Person::get_name(){
 }
 
 
-int main(){
-    int number_person, number_book;
-    cin >> number_person >> number_book;
-
-    Person* person_arr;
-    Book* book_arr;
-
-    person_arr = new Person[number_person];
-    book_arr = new Book[number_book];
-
+Person* person_arrayer(Person* person_arr, int number_person){
     for (int i = 0; i < number_person; i++) {
         string person;
         cin >> person;
@@ -81,11 +72,14 @@ int main(){
 
         person_arr[i] = man;
     }
+    return person_arr;
+}
 
     // for (int i = 0; i < number_person; i++) {
     //     cout << person_arr[i].get_name() << endl;
     // }
 
+Book* book_arrayer(Book* book_arr, int number_book){
     for (int i = 0; i < number_book; i++) {
         string input;
         string book_name, book_author;
@@ -122,7 +116,78 @@ int main(){
         book_arr[i] = current_book;
     }
 
-    for (int i = 0; i < number_book; i++) {
-        book_arr[i].displayDetails();
+    // for (int i = 0; i < number_book; i++) {
+    //     book_arr[i].displayDetails();
+    // }
+    return book_arr;
+}
+
+void query_date(int date_a, int date_b, Book*& book_arr){
+    int counter = 1;
+    for (size_t i = 0; i < sizeof(book_arr) - 1; i++) {
+        int current_year;
+        current_year = book_arr[i].get_publication_date();
+
+        if(current_year >= date_a and current_year <= date_b){
+            cout << "-----Query Date Match " << counter << "-----" << endl;
+            book_arr[i].displayDetails();
+            counter++;
+        }
     }
+    if (counter == 1) cout << "No Result" << endl;
+}
+
+void event_selector(int number_of_events, Book*& book_arr, Person*& person_arr){
+    for (size_t i = 0; i < number_of_events; i++) {
+        string command;
+        string input;
+        string second_statement, third_statement;
+        string second_string;
+        int second_int, third_int;
+
+        if(i == 0) cin.ignore();
+        getline(cin, input);
+
+        stringstream choice(input);
+        getline(choice, command, ' ');
+
+        if(command == "QUERY_AUTHOR" or command == "QUERY_TITLE"){
+            getline(choice, second_string);
+        }
+
+        else{
+            getline(choice, second_statement, ' ');
+            getline(choice, third_statement);
+
+            stringstream second(second_statement);
+            stringstream third(third_statement);
+
+            second >> second_int;
+            third >> third_int;
+        }
+        // The following lines of code now are the beginning of the choices
+
+        if(command == "QUERY_DATE") query_date(second_int, third_int, book_arr);
+
+    }
+}
+
+
+int main(){
+    int number_person, number_book;
+    cin >> number_person >> number_book;
+
+    Person* person_arr;
+    Book* book_arr;
+
+    person_arr = new Person[number_person];
+    book_arr = new Book[number_book];
+
+    person_arr = person_arrayer(person_arr, number_person);
+    book_arr = book_arrayer(book_arr, number_book);
+
+
+    int number_of_events;
+    cin >> number_of_events;
+    event_selector(number_of_events, book_arr, person_arr);
 }
