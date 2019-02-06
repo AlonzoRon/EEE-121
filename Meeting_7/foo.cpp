4a694fc3619e6573295d2ed359a6260d44c6abd6
AlonzoRon <45205131+AlonzoRon@users.noreply.github.com>
2019-02-06 10:23:18 +0800

    foo multiplication file

---
+++ Meeting_7/foo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int foo(int x, int y){
+    int ans = 0;
+    while(x > 0{
+        if(x % 2 == 1) {
+            ans = ans + y;
+        }
+    y *= 2;
+    x /= 2;
+    }
+    return  ans;
+}
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    cout << foo(x, y) << endl;
+}
